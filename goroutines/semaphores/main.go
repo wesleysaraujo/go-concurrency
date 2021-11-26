114ad644bdfe2577fc711d3e550ae879f72c4850
wesleyserafim <wesley@eguana.com.br>
2021-11-26 20:43:14 -0300

    Semaphores

---
+++ goroutines/semaphores/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	channel := make(chan int)
+	ok := make(chan bool)
+
+	go func () {
+		for i := 0; i < 20; i++ {
+			channel <- i
+		}
+		ok <- true
+	}()
+
+	go func() {
+		for i := 0; i < 20; i++ {
+			channel <- i
+		}
+		ok <- true
+	}()
+
+	go func() {
+		<-ok
+		<-ok
+		close(channel)
+	}()
+
+	for number := range channel {
+		fmt.Println(number)
+	}
+}
